9c6904df7a1d913edb8ee26501422d83c8c23191
Thabo <thabomoyo@avensure.com>
2024-07-28 18:52:18 +0100

    adding user_id to tasks table

--- database/migrations/2024_07_28_160723_create_tasks_table.php
+++ database/migrations/2024_07_28_160723_create_tasks_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('title');
             $table->text('description');
             $table->enum('status', ['to_do', 'cancelled', 'in_progress', 'completed'])->default('to_do');
