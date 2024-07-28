43345eaf3bd95611b4c192dbadd58efac3109637
Thabo <thabomoyo@avensure.com>
2024-07-28 17:15:09 +0100

    casting task status enum on Task Model

--- app/Models/Tasks.php
+++ app/Models/Tasks.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\TaskStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -14,6 +15,10 @@ class Tasks extends Model
         'title',
         'description',
         'status',
+    ];
+
+    protected $casts = [
+        'status' => TaskStatus::class,
     ];
 
     public function user(): BelongsTo
